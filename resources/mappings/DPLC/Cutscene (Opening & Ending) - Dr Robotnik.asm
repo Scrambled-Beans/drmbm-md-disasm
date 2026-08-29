e096ad502380c7bec5a0cb70ac3555ec1b42b06f
Scrambled Beans <scrambled-beans@outlook.com>
2026-08-29 21:26:11 +0100

    Robotnik's DPLC Files

---
+++ resources/mappings/DPLC/Cutscene (Opening & Ending) - Dr Robotnik.asm
@@ -0,0 +1,30 @@
+DPLC_Robotnik_Opening:	
+	; Offset, ID
+	dplcEntry	ArtUnc_Robotnik_0, 0
+	dplcEntry	ArtUnc_Robotnik_1, 1
+	dplcEntry	ArtUnc_Robotnik_2, 2
+	dplcEntry	ArtUnc_Robotnik_3, 3
+	dplcEntry	ArtUnc_Robotnik_4, 4
+	dplcEntry	ArtUnc_Robotnik_5, 5
+	dplcEntry	ArtUnc_Robotnik_6, 6
+	dplcEntry	ArtUnc_Robotnik_7, 7
+	dplcEntry	ArtUnc_Robotnik_8, 8
+	dplcEntry	ArtUnc_Robotnik_9, 9
+	dplcEntry	ArtUnc_Robotnik_10, 10
+	dplcEntry	ArtUnc_Robotnik_11, 11
+	dplcEntry	ArtUnc_Robotnik_12, 12
+	dplcEntry	ArtUnc_Robotnik_13, 13
+	dplcEntry	ArtUnc_Robotnik_14, 14
+	dplcEntry	ArtUnc_Robotnik_15, 15
+	dplcEntry	ArtUnc_Robotnik_16, 16
+	dplcEntry	ArtUnc_Robotnik_17, 17
+	dplcEntry	ArtUnc_Robotnik_18, 18
+	dplcEntry	ArtUnc_Robotnik_19, 19
+	dplcEntry	ArtUnc_Robotnik_20, 20
+	dplcEntry	ArtUnc_Robotnik_21, 21
+	dplcEntry	ArtUnc_Robotnik_22, 22
+	dplcEntry	ArtUnc_Robotnik_23, 23
+
+; ---------------------------------------------------------------------------
+
+	even
